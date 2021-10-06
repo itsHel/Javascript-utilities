5d017333b842fd2c51806bc9a4a9a20203f12468
itsHel <51739304+itsHel@users.noreply.github.com>
2021-10-06 16:18:17 +0200

    Update table.js

--- table/table.js
+++ table/table.js
@@ -172,17 +172,18 @@ function Table(data, nav, parentDiv, createRows, options = {}){
 
         if(options.saveFilters){
             activeFilters.forEach(filter => {
-                try{ document.querySelector("[data-type='" + filter + "']").classList.add("filter-active"); }catch(e){};
+                try{ document.querySelector("[data-type='" + filter + "']").classList.add("filter-active"); } catch(e){};
             });
         }
     }
 
     this.addSearch = function(){
         const searchIcon = '<svg viewBox="1 1 20 20"><path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"></path></svg>';
+        
         let placeholder = options.search.placeholder ?? "Search...";
         let searchButton = ((options.search.icon === false) ? "" : "<div class=btn-search>" + searchIcon + "</div>");
 
-        let searchHtml = "<div class=table-search-wrapper>" + searchButton + "<input id='search-" + options.id + "' type=search class=table-search autocorrect=off placeholder='" + placeholder + "'></div>";
+        let searchHtml = "<div class=table-search-wrapper>" + searchButton + "<input id='search-" + options.id + "' type=search class=table-search autocomplete=off placeholder='" + placeholder + "'></div>";
 
         if(options.search.id){
             document.querySelector("#" + options.search.id).innerHTML = searchHtml;
