1bff44c4665ee3710a4cb6a6cc7e4b981c5db531
itsHel <51739304+itsHel@users.noreply.github.com>
2021-11-18 15:19:09 +0100

    Update uti.js

--- uti.js
+++ uti.js
@@ -1,15 +1,14 @@
-// Return first real regex match - works with () - or null
-String.prototype.getMatch = function(regex){
+// Returns match or specified match or false
+String.prototype.getMatch = function(regex, index = 1){
     let match = this.match(regex);
 
     if(match){
         if(match.length == 1){
             return match[0];
         } else {
-            return match[1];
+            return match[index];
         }
     } else {
-        return null;
+        return false;
     }
 };
-
