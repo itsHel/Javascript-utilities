609dad383ce3c252baf05634b021a6abeed890ce
itsHel <51739304+itsHel@users.noreply.github.com>
2021-11-18 15:22:07 +0100

    Update notify.js

--- notify/notify.js
+++ notify/notify.js
@@ -8,6 +8,7 @@ function notify(text, type = "", delay = 4000){
 
     let ico = "";
     let headerText = "Notify";
+
     switch(type){
         case "warn":
             ico = '<svg viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"/><circle cx="12" cy="19" r="2"/><path d="M10 3h4v12h-4z"/></svg>';
@@ -19,7 +20,7 @@ function notify(text, type = "", delay = 4000){
             break;
         case "load":
             ico = '<svg viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"/><path d="M12 6v3l4-4-4-4v3c-4.42 0-8 3.58-8 8 0 1.57.46 3.03 1.24 4.26L6.7 14.8c-.45-.83-.7-1.79-.7-2.8 0-3.31 2.69-6 6-6zm6.76 1.74L17.3 9.2c.44.84.7 1.79.7 2.8 0 3.31-2.69 6-6 6v-3l-4 4 4 4v-3c4.42 0 8-3.58 8-8 0-1.57-.46-3.03-1.24-4.26z"/></svg>';
-            headerText = "Loading";
+            headerText = "Nahrávání";
             text = "&nbsp;<img src='img/loading.gif'>"
             break;
         case "":
